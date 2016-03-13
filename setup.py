0e4d9618d4d5adceb547d1e5cca851e0cfc32433
Ben Einaudi <benjamin.einaudi@orange.com>
2016-03-13 14:24:11 +0100

    Update project configuration files - Add .gitignore file to filter undeisred files (compiled, packaging and IDE) - Add requirement in both requirements.txt and setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(name='pyorangecloud',
       version='1.0',
       py_modules=['pyorangecloud'],
+      url='https://github.com/Orange-OpenSource/pyorangecloud',
+      classifiers=[
+          "Programming Language :: Python",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Communications",
+      ],
+      install_requires=[requirement.rstrip(' \r\n') for requirement in open('requirements.txt').readlines()],
       )
